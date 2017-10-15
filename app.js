d9efcfe63d02ac0281a68c183835310a9a9f6a50
Eddie Yepez <eddieyepez@yahoo.com>
2017-10-15 00:21:22 -0400

    New css
    js changes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
 
-  $.getJSON("api.openweathermap.org/data/2.5/weather?lat={"+data.lat+"}&lon={"+data.lon+"}", function(weather){
+	//console.log(data);
+  	$.getJSON("api.openweathermap.org/data/2.5/weather?lat={"+data.lat+"}&lon={"+data.lon+"}", function(weather){
 
-    console.log(weather);
+    	//console.log(weather);
+    	$("#demo").html(weather);
     
-  });
+  	});
 
 });
 

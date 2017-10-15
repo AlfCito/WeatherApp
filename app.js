5832c4a991af260283215dfa9a7c1c4a15a6f0d4
Eddie Yepez <eddieyepez@yahoo.com>
2017-10-15 00:57:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $.getJSON("https://ip-api.com/json/?callback=?", function(data) {
 
 	//console.log(data);
-  	$.getJSON("https://api.openweathermap.org/data/2.5/weather?lat={"+data.lat+"}&lon={"+data.lon+"}&APPID={4823e0379d7d603fb2cbfdbad5c5842e}", weather );
+  	$.getJSON("api.openweathermap.org/data/2.5/weather?lat={"+data.lat+"}&lon={"+data.lon+"}&APPID={4823e0379d7d603fb2cbfdbad5c5842e}", weather );
 
 });
 

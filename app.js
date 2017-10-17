df864ed3c3aa6a82739a0eabd0a75df77896960a
Eddie Yepez <eddieyepez@yahoo.com>
2017-10-17 00:00:10 -0400

    working
    working

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
 $(document).ready(function($){
 
+	
+	var lat ='';
+	var lon ='';
+
 	$.ajax({
 		type: 'GET',
-		url: 'https://maps.googleapis.com/maps/api/geocode/output?parameters',
-		//url: 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCFWka7xeWSpSN_RVzKbl0lZyW4rZqOS9A',
+		url: "http://ip-api.com/json",
 		crossDomain: true,
 		dataType: 'json',
 		success: function(data){
 			console.log('success', data);
+			lat = data.lat;
+			lon = data.lon;
+			console.log('lat: '+ lat + 'lon: '+ lon );
+
+			weather();
 		},
 		error: function(e){
-			console.log('error', e);
+			console.log('Error Finding your Location', e);
 		}
-	})
-	/*
-	$.getJSON("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCFWka7xeWSpSN_RVzKbl0lZyW4rZqOS9A", function(data) {
-
-		console.log(data);
-	  	//$.getJSON("api.openweathermap.org/data/2.5/weather?lat={"+data.lat+"}&lon={"+data.lon+"}&APPID=4823e0379d7d603fb2cbfdbad5c5842e?callback=weather");
-
 	});
 
+	
 	function weather(weather){
 
-		//console.log(weather);
-		$("#demo").html(weather);
+		var appKey = "4823e0379d7d603fb2cbfdbad5c5842e";
+
+		$.ajax({
+			type: 'GET',
+			url: "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+appKey+"",
+			crossDomain: true,
+			dataType: 'json',
+			success: function(data){
+				console.log('success', data);
+			},
+			error: function(e){
+				console.log('error', e);
+			}
+		});
+
+		//$.getJSON("api.openweathermap.org/data/2.5/weather?lat={"+lat+"}&lon={"+lon+"}&appid={"+appKey+"}?callback=weather");
+		
+		//$("#demo").html(weather);
 
 	};
-	*/
+	
 
 });
 

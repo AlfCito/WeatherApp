6f2f8c0487119a715f7b61f0a096915d39a70e69
Eddie Yepez <eddieyepez@yahoo.com>
2017-10-16 22:47:33 -0400

    .
    .

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-$.getJSON("https://ip-api.com/json/?callback=?", function(data) {
+$(function(){
 
-	//console.log(data);
-  	$.getJSON("https://api.openweathermap.org/data/2.5/weather?lat={"+data.lat+"}&lon={"+data.lon+"}", weather );
+	$.ajax({
+		type: 'GET',
+		url: 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCFWka7xeWSpSN_RVzKbl0lZyW4rZqOS9A',
+		success: function(data){
+			console.log('success', data);
+		}
+
+	})
+/*
+	$.getJSON("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCFWka7xeWSpSN_RVzKbl0lZyW4rZqOS9A", function(data) {
+
+		console.log(data);
+	  	//$.getJSON("api.openweathermap.org/data/2.5/weather?lat={"+data.lat+"}&lon={"+data.lon+"}&APPID=4823e0379d7d603fb2cbfdbad5c5842e?callback=weather");
+
+	});
+
+	function weather(weather){
+
+		//console.log(weather);
+		$("#demo").html(weather);
+
+	};*/
 
 });
-
-function weather(weather){
-
-	//console.log(weather);
-	$("#demo").html(weather);
-
-};
 
 
